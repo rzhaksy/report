d7ba2520f6d1bab6314e21c6ab28922565d4f4bc
rzhaksy <143612442+rzhaksy@users.noreply.github.com>
2023-09-01 14:58:34 +0600

    Update main.js

--- main.js
+++ main.js
@@ -19,12 +19,10 @@ var myFrame = document.getElementById('myframe');
 	}
 	else if (i == 3){
 	myFrame.src='https://grafana.o3.ru/d/WwgP6CBVk/28-processing-time-and-sla?orgId=1&var-Cluster=%D0%9A%D0%BB%D0%B0%D1%81%D1%82%D0%B5%D1%80%20%D0%9A%D0%B0%D0%B7%D0%B0%D1%85%D1%81%D1%82%D0%B0%D0%BD&var-HubType=All&var-SortCenter=1020000310007000&var-action=All&var-article_type=All&var-threshold=20&var-mode=1&var-tracing=%7B%22dashboard%22:%20%22%2328.%20Processing%20Time%20%26%20SLA%22,%20%20%22uid%22:%22WwgP6CBVk%22,%20%22user_id%22:%200,%20%22user_login%22:%20%22%22,%20%22timefilter_from%22:%201692174004887,%20%22timefilter_to%22:%201692346804887%7D&var-event_user_login=&var-delivery_schema=All&var-is_return=All&var-is_zone_courier_give_out=All&var-is_zone_keeping=All&from=now-24h&to=now';
+	
 	i=0;
 	}
 
 
-}
 
 setInterval(showrep, 5000);
-
-</script>
