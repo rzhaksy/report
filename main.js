8e4a1864f26208b7bbaa443b6390784f4116fb15
rzhaksy <143612442+rzhaksy@users.noreply.github.com>
2023-09-03 17:31:42 +0600

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function showrep(){
 	i++;
 	}
 	else if (i == 1){
-	myframe.src = 'http://*sre-bot.sre.prod.s.o3.ru/?//grafana.o3.ru/d/xKRiv8jMk/internal-tv-slo?orgId=1&refresh=5m&from=now-5m&to=now';
+	myframe.src = 'https://grafana.o3.ru/d-solo/fjVlUU04z/late-shipment?orgId=1&refresh=5m&var-flow_type=transit&var-warehouse=%D0%90%D0%9B%D0%9C%D0%90%D0%A2%D0%AB_%D0%9C%D0%A0%D0%A4%D0%A6&var-name_groupped_1=%D0%9D%D0%B5+%D0%B2%D1%8B%D1%87%D0%B8%D1%82%D0%B0%D0%BD%D1%8B+%D0%B8%D0%B7+%D1%82%D1%80%D0%B0%D0%BD%D0%B7%D0%B8%D1%82%D0%BD%D0%BE%D0%B3%D0%BE+%D0%B3%D1%80%D1%83%D0%B7%D0%B0&var-is_well_read_link=%D0%9D%D0%B5+%D0%B2%D1%8B%D1%87%D0%B8%D1%82%D0%B0%D0%BB%D0%B8+%D0%B8%D0%B7+%D1%82%D1%80%D0%B0%D0%BD%D0%B7%D0%B8%D1%82%D0%BD%D0%BE%D0%B3%D0%BE+%D0%BA%D0%BE%D0%BD%D1%82%D0%B5%D0%B9%D0%BD%D0%B5%D1%80%D0%B0&var-transit_type_link=%D0%A1+%D0%B2%D1%8B%D1%87%D0%B8%D1%82%D0%BA%D0%BE%D0%B9&var-ls_total_postings=169753&var-hours=6&var-minutes=360&from=1693718521347&to=1693740121347&panelId=51';
 	i++;
 
 	}
@@ -17,7 +17,7 @@ function showrep(){
 	i++;
 	}
 	else if (i == 3){
-	myframe.src = 'https://grafana.o3.ru/d/WwgP6CBVk/28-processing-time-and-sla?orgId=1&var-Cluster=%D0%9A%D0%BB%D0%B0%D1%81%D1%82%D0%B5%D1%80%20%D0%9A%D0%B0%D0%B7%D0%B0%D1%85%D1%81%D1%82%D0%B0%D0%BD&var-HubType=All&var-SortCenter=1020000310007000&var-action=All&var-article_type=All&var-threshold=20&var-mode=1&var-tracing=%7B%22dashboard%22:%20%22%2328.%20Processing%20Time%20%26%20SLA%22,%20%20%22uid%22:%22WwgP6CBVk%22,%20%22user_id%22:%200,%20%22user_login%22:%20%22%22,%20%22timefilter_from%22:%201692174004887,%20%22timefilter_to%22:%201692346804887%7D&var-event_user_login=&var-delivery_schema=All&var-is_return=All&var-is_zone_courier_give_out=All&var-is_zone_keeping=All&from=now-24h&to=now';
+	myframe.src = 'https://grafana.o3.ru/d-solo/fjVlUU04z/late-shipment?orgId=1&refresh=5m&var-flow_type=transit&var-warehouse=%D0%90%D0%9B%D0%9C%D0%90%D0%A2%D0%AB_%D0%9C%D0%A0%D0%A4%D0%A6&var-name_groupped_1=%D0%9D%D0%B5+%D0%B2%D1%8B%D1%87%D0%B8%D1%82%D0%B0%D0%BD%D1%8B+%D0%B8%D0%B7+%D1%82%D1%80%D0%B0%D0%BD%D0%B7%D0%B8%D1%82%D0%BD%D0%BE%D0%B3%D0%BE+%D0%B3%D1%80%D1%83%D0%B7%D0%B0&var-is_well_read_link=%D0%9D%D0%B5+%D0%B2%D1%8B%D1%87%D0%B8%D1%82%D0%B0%D0%BB%D0%B8+%D0%B8%D0%B7+%D1%82%D1%80%D0%B0%D0%BD%D0%B7%D0%B8%D1%82%D0%BD%D0%BE%D0%B3%D0%BE+%D0%BA%D0%BE%D0%BD%D1%82%D0%B5%D0%B9%D0%BD%D0%B5%D1%80%D0%B0&var-transit_type_link=%D0%A1+%D0%B2%D1%8B%D1%87%D0%B8%D1%82%D0%BA%D0%BE%D0%B9&var-ls_total_postings=169753&var-hours=6&var-minutes=360&from=1693718521347&to=1693740121347&panelId=51';
 	
 	i=0;
 	}
